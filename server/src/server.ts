5831da99898a1f6adc5b3755e54f1fd42bf34f15
Gustavo Azevedo <gustavosazevedo@gmail.com>
2022-09-14 02:51:10 -0300

    setting up database with prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,20 +2,34 @@ import express from "express";
 
 const app = express()
 
-app.get('/ads', (req, res) => res.json([
-  {
-    id: 1,
-    name: "Anúncio 1"
-  },
-  {
-    id: 2,
-    name: "Anúncio 2"
-  },
-  {
-    id: 3,
-    name: "Anúncio 3"
-  },
-]))
+app.get('/games', (request, response) => response.json([]))
+
+app.post('/ads', (request, response) => response.status(201).json([]))
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id
+
+  return response.json([
+    {
+      id: 1,
+      name: "Anúncio 1"
+    },
+    {
+      id: 2,
+      name: "Anúncio 2"
+    },
+    {
+      id: 3,
+      name: "Anúncio 3"
+    },
+  ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adId = request.params.id
+
+  return response.json([])
+})
 
 app.listen(3333, () => {
   console.log('server is running at http://localhost:3333')
